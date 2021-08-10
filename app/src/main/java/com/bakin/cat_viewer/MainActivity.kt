ad8a2c96ae12a7f27b02b06e85b9e85cbbd1c17e
donebd <dinil.bd@mail.ru>
2021-08-11 01:22:46 +0300

    add image magnification

--- app/src/main/java/com/bakin/cat_viewer/MainActivity.kt
+++ app/src/main/java/com/bakin/cat_viewer/MainActivity.kt
@@ -8,23 +8,30 @@ import com.squareup.picasso.Picasso
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var catView : ImageView
-    lateinit var getCatButton : Button
-    
+    lateinit var catView: ImageView
+    lateinit var getCatButton: Button
+    private var isImageScaled = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        catView  = findViewById(R.id.image)
+        catView = findViewById(R.id.image)
         getCatButton = findViewById(R.id.getCatButton)
 
         getCatButton.setOnClickListener {
             Thread {
                 val url = Cat.getUrl()
-                    runOnUiThread {
-                        Picasso.get().load(url).resize(1080,720).into(catView);
-                    }
+                runOnUiThread {
+                    Picasso.get().load(url).resize(1080, 720).into(catView);
+                }
             }.start()
+        }
+
+        catView.setOnClickListener {
+            val scale = if (!isImageScaled) 1.4f else 1f
+            it.animate().scaleX(scale).scaleY(scale).setDuration(500)
+            isImageScaled = !isImageScaled
         }
     }
 
