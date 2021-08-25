7f339ad8e32fabe14ab60b008c8d86f503464992
done_bd <dinil.bd@mail.ru>
2021-08-25 19:53:11 +0300

    Ui fix

--- app/src/main/java/com/bakin/cat_viewer/getCat.kt
+++ app/src/main/java/com/bakin/cat_viewer/getCat.kt
@@ -6,7 +6,6 @@ import java.io.BufferedReader
 import java.io.IOException
 import java.io.InputStreamReader
 import java.lang.IndexOutOfBoundsException
-import java.net.URI
 import java.net.HttpURLConnection
 import java.net.URL
 
